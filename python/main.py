b896bea530348199d4b4896662a26d3ba5117a2c
KevinFham <kevin.pha01@gmail.com>
2026-04-08 13:58:09 -0500

    buffer and resend

--- python/main.py
+++ python/main.py
@@ -15,14 +15,24 @@ PORT = 65432
 def rx(pkt_loss_chance=1.0):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as r:
         r.bind((RX_ADDR, PORT))
+        pkt_buffer = []
+
         while True:
             data, addr = r.recvfrom(1024)
             data_parsed = data.decode("utf-8").split(" ")
             i_data = data_parsed[0]
-            print(f'rx: {data_parsed}')
+            # print(f'rx: {data_parsed}')
 
             # Return ACK
             if random.random() < pkt_loss_chance:
+
+                # Buffer; EOF packet, push out buffer
+                if data_parsed[1] == '-1':
+                    sorted(pkt_buffer)
+                    print(" ".join([w[-1].replace("\n","") for w in pkt_buffer]))
+                    pkt_buffer.clear()
+                else:
+                    pkt_buffer.append(data_parsed)
                 r.sendto(bytes(f'{i_data} 1 ACK', "utf-8"), addr)
 
 """
@@ -30,19 +40,21 @@ def rx(pkt_loss_chance=1.0):
 """
 def tx(pkt_seq=[]):
     with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as t:
-        t.settimeout(1.0)
+        t.settimeout(0.4)
         for pkt in pkt_seq:
-            t_start = time.time()
-            t.sendto(bytes(pkt, "utf-8"), (RX_ADDR, PORT))
+            while True:
+                t.sendto(bytes(pkt, "utf-8"), (RX_ADDR, PORT))
+                t_start = time.time()
 
-            # Await ACK
-            try:
-                data, addr = t.recvfrom(1024)
-                t_d = (time.time() - t_start) * 1000
-                data_parsed = data.decode("utf-8").split(" ")
-                print(f'tx: {data_parsed} {t_d:.2f} ms')
-            except socket.timeout:
-                print("timeout")
+                # Await ACK, retry if UNACK
+                try:
+                    data, addr = t.recvfrom(1024)
+                    t_d = (time.time() - t_start) * 1000
+                    data_parsed = data.decode("utf-8").split(" ")
+                    # print(f'tx: {data_parsed} {t_d:.2f} ms')
+                    break
+                except socket.timeout:
+                    print("timeout")
 
 
 if __name__ == '__main__':

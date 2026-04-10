c4a9c6ba3319466a14c97c9209c5c29b8c29f0bd
KevinFham <kevin.pha01@gmail.com>
2026-04-10 17:09:18 -0500

    threading wip

--- c/main.c
+++ c/main.c
@@ -18,8 +18,16 @@ unsigned int REACK_COUNT = 0;
 /*
  * Reciever Socket
  */
-void* rx(float pktLossChance) {
+struct rxArgs {
+    float   *pktLossChance;
+};
+
+void* rx(void* arg) {
+    struct rxArgs *rxA = arg;
+    printf("%f", arg->pktLossChance);
+
     struct sockaddr_in rxAddr, txAddr;
+    socklen_t len = sizeof(txAddr);
     char buffer[MAX_BUFFER];
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -36,11 +44,12 @@ void* rx(float pktLossChance) {
     }
 
     // Recieve Packets
-    socklen_t len = sizeof(txAddr);
-    printf("e");
-    recvfrom(sockfd, (char*)buffer, MAX_BUFFER, MSG_WAITALL, (struct sockaddr*)&txAddr, &len);
-    printf("f");
-    printf("%s", buffer);
+    while (1) {
+        printf("e");
+        int recv = recvfrom(sockfd, (char*)buffer, MAX_BUFFER, MSG_WAITALL, (struct sockaddr*)&txAddr, &len);
+        printf("f %i", recv);
+        printf("%s", buffer);
+    }
 
 }
 
@@ -49,6 +58,7 @@ void* rx(float pktLossChance) {
  */
 void* tx(char* pktSeq[][PACKET_SIZE], float timeoutms) {
     struct sockaddr_in rxAddr;
+    socklen_t len = sizeof(rxAddr);
     char buffer[MAX_BUFFER];
     int sockfd;
     if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
@@ -61,7 +71,10 @@ void* tx(char* pktSeq[][PACKET_SIZE], float timeoutms) {
     rxAddr.sin_addr.s_addr = inet_addr(RX_ADDR);
 
     // Send Packets
-    socklen_t len = sizeof(rxAddr);
+    char message[MAX_BUFFER] = "cheese";
+    
+    int sent = sendto(sockfd, message, strlen(message), 0, (struct sockaddr*)&rxAddr, len);
+    printf("sent %i", sent);
 }
 
 /*
@@ -86,7 +99,11 @@ int main(int argc, char** argv) {
     }
 
     // Daemonize Receiver
-    rx(1.0f);
+    pthread_t rxThread;
+    struct rxArgs *rxA = calloc(1, sizeof(*rxA));
+    rxA->pktLossChance = 1.0f;
+    pthread_create(&rxThread, NULL, rx, &rxA);
+    //rx(1.0f);
 
     // Transmit Packets
     char* packets[][3] = {{"-1", "1", "ACK"}};
